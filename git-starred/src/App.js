f3c3688bd2b450bf1a5f29a813b1319e96942053
Ali El-Sherbiny <alielsherbiny142@gmail.com>
2021-09-05 17:45:42 +0200

    Displaying the required data from the fetched data without styling.
    Add displayed key to state object to show number of displayed cards (displaying 30 until now)

--- git-starred/src/App.js
+++ git-starred/src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
 import React, { Component } from 'react'
-import CardsList from './Components/Cards'
+import CardsList from './Components/CardsList'
 import axios from 'axios'
 
 class App extends Component {
 state = {
   total:'',
+  displayed:0,
   repos:[]
 }
   componentDidMount()
   {
     axios.get ('&page=1')
-    .then ((repos)=>(this.setState({total:repos.data.total_count,repos:repos.data.items},()=>(console.log(this.state)))))
-    //(this.setState({total:repos.data.total_count,repos:repos.data.items},()=>(console.log(this.state))))
-    
+    .then ((repos)=>(this.setState((prevState)=>({total:repos.data.total_count,displayed:prevState.displayed+30,repos:repos.data.items}),()=>(console.log(this.state)))))  
   }
   render(){
   return (
     <div className="App">
-      {/*this.state.repos*/}
-      <CardsList repos={[]}/>
+      {
+      this.state.total?
+      (<CardsList repos={this.state.repos}/>):(null)
+      }
     </div>
   );
 }
--- git-starred/src/Components/Card.js
+++ git-starred/src/Components/Card.js
@@ -4,12 +4,23 @@ import { Component } from "react";
 class Card extends Component
 {
 render()
-{
-
+{ 
+  let months = Math.floor(((Date.now()-Date.parse(this.props.repo.updated_at))/1000)/60/60/24/30) ; 
     return (
 
         <li>
-             {this.props.repo}
+            <ul>
+             <li>{'REPO NAME:       '+this.props.repo.name}</li>
+             <li>{'USER NAME:       '+this.props.repo.owner.login}</li>
+             <li>{'REPO DESC:       '+this.props.repo.description}</li>
+             <li>{'NO. OF OPEN ISSUES:       '+this.props.repo.open_issues_count}</li>
+             <li>{'NO. OF STARS:       '+this.props.repo.stargazers_count}</li>
+             <li>{'NO. OF MONTHS SINCE LAST UPDATED:       '+months}</li>   
+             <li>{}</li>    
+             <li>{ <img src={`${this.props.repo.owner.avatar_url}.png`} alt="User Profile Pic" width="100" height="100" /> } </li>
+             <li>{}</li>
+             <li>{}</li>
+             </ul>
         </li>
     )
 }
