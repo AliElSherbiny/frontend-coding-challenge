823790dcafa2b0c68796bb81894b93d73f183387
Ali El-Sherbiny <alielsherbiny142@gmail.com>
2021-09-05 15:44:57 +0200

    importing axios library for making http requests
    initial getting data and updating the state upon fetching it

--- git-starred/src/App.js
+++ git-starred/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import React, { Component } from 'react'
 import CardsList from './Components/Cards'
+import axios from 'axios'
 
-function App() {
+class App extends Component {
+state = {
+  total:'',
+  repos:[]
+}
+  componentDidMount()
+  {
+    axios.get ('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+    .then ((repos)=>(this.setState({total:repos.data.total_count,repos:repos.data.items},()=>(console.log(this.state)))))
+    //(this.setState({total:repos.data.total_count,repos:repos.data.items},()=>(console.log(this.state))))
+    
+  }
+  render(){
   return (
     <div className="App">
-      <CardsList/>
+      {/*this.state.repos*/}
+      <CardsList repos={[]}/>
     </div>
   );
+}
 }
 
 export default App;
